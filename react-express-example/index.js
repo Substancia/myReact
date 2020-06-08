51a855697655ac978916a7540f1e066fa8a4ab70
sentientasins <jibin@backprop.in>
2020-06-08 15:44:45 +0530

    learned react-express combination

---
+++ react-express-example/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = [{name: "item1", id: "1"}, {name: "item2", id: "2"}, {name: "item3", id: "3"}];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
